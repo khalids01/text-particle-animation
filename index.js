9ede68b1a72139cb96783f9bed57623da5514811
khalids01 <khalids8774@gmail.com>
2023-01-27 00:13:23 +0600

    basic canvas dynamic text position and line

--- index.js
+++ index.js
@@ -1,132 +1,63 @@
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-const adjustX = 10;
-const adjustY = 30;
-const text = 'Khalid'
+window.addEventListener('load', function () {
+    const textInput = document.getElementById('textInput')
+    const canvas = document.querySelector('canvas')
+    const ctx = canvas.getContext('2d')
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
 
-const mouse = {
-    x: 0,
-    y: 0,
-    radius: 100
-}
+    ctx.lineWidth = 3
+    ctx.strokeStyle = 'red'
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, 0)
+    ctx.lineTo(canvas.width / 2, canvas.height)
+    ctx.stroke()
 
-let particleArray = []
+    ctx.strokeStyle = 'green'
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height / 2)
+    ctx.lineTo(canvas.width, canvas.height / 2)
+    ctx.stroke()
 
-window.addEventListener('mousemove', (e) => {
-    mouse.x = e.clientX
-    mouse.y = e.clientY
+    const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height)
+    gradient.addColorStop(0.3, 'teal')
+    gradient.addColorStop(0.5, 'fuchsia')
+    gradient.addColorStop(0.7, 'purple')
+    ctx.fillStyle = gradient
+    ctx.font = '80px Helvetica'
+    ctx.textBaseline = 'middle'
+    ctx.strokeStyle = 'teal'
+    ctx.textAlign = 'center'
+
+    const maxTextWidth = canvas.width * 0.5;
+    const lineHeight = 80;
+
+    function wrapText(text) {
+        let linesArray = [];
+        let lineCounter = 0;
+        let line = '';
+        let words = text.split(' ');
+        for (let i = 0; i < words.length; i++) {
+            let testLine = line + words[i] + ' ';
+            if (ctx.measureText(testLine).width > maxTextWidth) {
+                line = words[i] + ' ';
+                lineCounter++
+            } else {
+                line = testLine
+            }
+            linesArray[lineCounter] = line
+        }
+        let textHeight = lineHeight * lineCounter
+        let textY = canvas.height / 2 - textHeight / 2
+
+        linesArray.forEach((el, index) => {
+            ctx.fillText(el, canvas.width / 2, textY + (index * lineHeight))
+        })
+
+        console.log(linesArray)
+    }
+
+    textInput.addEventListener('keyup', function (e) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        wrapText(e.target.value)
+    })
 })
-
-window.onresize = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    init();
-}
-
-ctx.fillStyle = 'white'
-ctx.font = '30px Verdana'
-ctx.fillText(text, 0, 30)
-const textCoordinates = ctx.getImageData(0, 0, 100, 100)
-
-
-class Particle {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.size = 3;
-        this.baseX = this.x;
-        this.baseY = this.y;
-        this.density = (Math.random() * 40) + 10
-    }
-
-    draw() {
-        ctx.beginPath()
-        ctx.fillStyle = 'white';
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
-        ctx.closePath()
-        ctx.fill()
-    }
-
-    update() {
-        let dx = mouse.x - this.x;
-        let dy = mouse.y - this.y;
-
-        let distance = Math.sqrt(dx * dx + dy * dy)
-        let forceDirectionX = dx / distance * 3
-        let forceDirectionY = dy / distance * 3
-        let maxDistance = mouse.radius
-        let force = (maxDistance - distance) / maxDistance;
-        let directionX = forceDirectionX * force * this.density
-        let directionY = forceDirectionY * force * this.density
-
-        if (distance < mouse.radius) {
-            this.x -= directionX
-            this.y -= directionY
-        } else {
-            if (this.x !== this.baseX) {
-                let dx = this.x - this.baseX
-                this.x -= dx / 10;
-            }
-            if (this.y !== this.baseY) {
-                let dy = this.y - this.baseY
-                this.y -= dy / 10
-            }
-        }
-    }
-}
-
-// draw()
-
-console.log(textCoordinates)
-
-function init() {
-    particleArray = []
-    for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
-        for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
-            if (textCoordinates.data[(y * 4 * textCoordinates.width) + (x * 4) + 3] > 128) {
-                let positionX = x + adjustX;
-                let positionY = y + adjustY;
-                particleArray.push(new Particle(positionX * 8, positionY * 8))
-            }
-        }
-    }
-}
-init()
-
-
-function animate() {
-    ctx?.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (let i = 0; i < particleArray.length; i++) {
-        particleArray[i].draw()
-        particleArray[i].update()
-    }
-    connect()
-    requestAnimationFrame(animate)
-}
-
-animate()
-
-function connect() {
-    let opacityValue = 1;
-    for (let a = 0; a < particleArray.length; a++) {
-        for (let b = a; b < particleArray.length; b++) {
-            let dx = particleArray[a].x - particleArray[b].x;
-            let dy = particleArray[a].y - particleArray[b].y;
-            let distance = Math.sqrt(dx * dx + dy * dy);
-
-            opacityValue = 1 - (distance / 18)
-
-            if (distance < 20) {
-                ctx.strokeStyle = `rgba(255, 255, 255, ${opacityValue})`;
-                ctx.lineWidth = 2
-                ctx.beginPath();
-                ctx.moveTo(particleArray[a].x, particleArray[a].y);
-                ctx.lineTo(particleArray[b].x, particleArray[b].y)
-                ctx.stroke()
-            }
-        }
-    }
-}
